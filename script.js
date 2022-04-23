8493e8ee2b6554bfad9048a71da47d5cd43a00bb
vinav7400 <88160015+vinav7400@users.noreply.github.com>
2022-04-23 15:02:32 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function concat(){
+    first_name = "Ranbir";
+    last_name = "Kapoor";
+    first_number = 54;
+    second_number = 24;
+    full_name =  "Ranbir"+"Kapoor";
+    console.log(full_name);
+    full_number = first_number + second_number;
+    console.log(full_number);
+}
